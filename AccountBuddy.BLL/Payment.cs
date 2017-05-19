fc84ece9b380ab76e496ece1cc3c73dfac8a40c5
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-05-19 14:48:36 +0530

    BLL Created

---
+++ AccountBuddy.BLL/Payment.cs
@@ -0,0 +1,493 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountBuddy.Common;
+
+namespace AccountBuddy.BLL
+{
+    public class Payment : INotifyPropertyChanged
+    {
+        #region Fields
+        private long _Id;
+        private string _EntryNo;
+        private DateTime? _PaymentDate;
+        private int? _LedgerId;
+        private string _PaymentMode;
+        private decimal? _Amount;
+        private string _RefNo;
+        private string _Status;
+        private decimal? _ExtraCharge;
+        private string _ChequeNo;
+        private DateTime? _ChequeDate;
+        private DateTime? _ClearDate;
+        private string _Particulars;
+        private string _PayTo;
+        private string _VoucherNo;
+        private string _LedgerName;
+
+        private Ledger _PLedger;
+
+        private PaymentDetail _PDetail;
+        private ObservableCollection<PaymentDetail> _PDetails;
+
+        private string _SearchText;
+        #endregion
+
+        #region  Property
+        public long Id
+        {
+            get
+            {
+                return _Id;
+            }
+            set
+            {
+                if (_Id != value)
+                {
+                    _Id = value;
+                    NotifyPropertyChanged(nameof(Id));
+                }
+            }
+        }
+        public string EntryNo
+        {
+            get
+            {
+                return _EntryNo;
+            }
+            set
+            {
+                if (_EntryNo != value)
+                {
+                    _EntryNo = value;
+                    NotifyPropertyChanged(nameof(EntryNo));
+                }
+            }
+        }
+        public DateTime? PaymentDate
+        {
+            get
+            {
+                return _PaymentDate;
+            }
+            set
+            {
+                if (_PaymentDate != value)
+                {
+                    _PaymentDate = value;
+                    NotifyPropertyChanged(nameof(PaymentDate));
+                }
+            }
+        }
+        public int? LedgerId
+        {
+            get
+            {
+                return _LedgerId;
+            }
+            set
+            {
+                if (_LedgerId != value)
+                {
+                    _LedgerId = value;
+                    NotifyPropertyChanged(nameof(LedgerId));
+                }
+            }
+        }
+        public string PaymentMode
+        {
+            get
+            {
+                return _PaymentMode;
+            }
+            set
+            {
+                if (_PaymentMode != value)
+                {
+                    _PaymentMode = value;
+                    NotifyPropertyChanged(nameof(PaymentMode));
+                }
+            }
+        }
+        public decimal? Amount
+        {
+            get
+            {
+                return _Amount;
+            }
+            set
+            {
+                if (_Amount != value)
+                {
+                    _Amount = value;
+                    NotifyPropertyChanged(nameof(Amount));
+                }
+            }
+        }
+        public string RefNo
+        {
+            get
+            {
+                return _RefNo;
+            }
+            set
+            {
+                if (_RefNo != value)
+                {
+                    _RefNo = value;
+                    NotifyPropertyChanged(nameof(RefNo));
+                }
+            }
+        }
+        public string Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    NotifyPropertyChanged(nameof(Status));
+                }
+            }
+        }
+        public Nullable<decimal> ExtraCharge
+        {
+            get
+            {
+                return _ExtraCharge;
+            }
+            set
+            {
+                if (_ExtraCharge != value)
+                {
+                    _ExtraCharge = value;
+                    NotifyPropertyChanged(nameof(ExtraCharge));
+                }
+            }
+        }
+        public string ChequeNo
+        {
+            get
+            {
+                return _ChequeNo;
+            }
+            set
+            {
+                if (_ChequeNo != value)
+                {
+                    _ChequeNo = value;
+                    NotifyPropertyChanged(nameof(ChequeNo));
+                }
+            }
+        }
+        public Nullable<System.DateTime> ChequeDate
+        {
+            get
+            {
+                return _ChequeDate;
+            }
+            set
+            {
+                if (_ChequeDate != value)
+                {
+                    _ChequeDate = value;
+                    NotifyPropertyChanged(nameof(ChequeDate));
+                }
+            }
+        }
+        public Nullable<System.DateTime> ClearDate
+        {
+            get
+            {
+                return _ClearDate;
+            }
+            set
+            {
+                if (_ClearDate != value)
+                {
+                    _ClearDate = value;
+                    NotifyPropertyChanged(nameof(ClearDate));
+                }
+            }
+        }
+        public string Particulars
+        {
+            get
+            {
+                return _Particulars;
+            }
+            set
+            {
+                if (_Particulars != value)
+                {
+                    _Particulars = value;
+                    NotifyPropertyChanged(nameof(Particulars));
+                }
+            }
+        }
+        public string PayTo
+        {
+            get
+            {
+                return _PayTo;
+            }
+            set
+            {
+                if (_PayTo != value)
+                {
+                    _PayTo = value;
+                    NotifyPropertyChanged(nameof(PayTo));
+                }
+            }
+        }
+        public string VoucherNo
+        {
+            get
+            {
+                return _VoucherNo;
+            }
+            set
+            {
+                if (_VoucherNo != value)
+                {
+                    _VoucherNo = value;
+                    NotifyPropertyChanged(nameof(VoucherNo));
+                }
+            }
+        }
+
+        public string LedgerName
+        {
+            get
+            {
+                return _LedgerName;
+            }
+            set
+            {
+                if (_LedgerName != value)
+                {
+                    _LedgerName = value;
+                    NotifyPropertyChanged(nameof(LedgerName));
+                }
+            }
+        }
+
+
+        public PaymentDetail PDetail
+        {
+            get
+            {
+                if (_PDetail == null) _PDetail = new PaymentDetail();
+                return _PDetail;
+            }
+            set
+            {
+                if (_PDetail != value)
+                {
+                    _PDetail = value;
+                    NotifyPropertyChanged(nameof(PDetail));
+                }
+            }
+        }
+
+        public ObservableCollection<PaymentDetail> PDetails
+        {
+            get
+            {
+                if (_PDetails == null) _PDetails = new ObservableCollection<PaymentDetail>();
+                return _PDetails;
+            }
+            set
+            {
+                if (_PDetails != value)
+                {
+                    _PDetails = value;
+                    NotifyPropertyChanged(nameof(PDetails));
+                }
+            }
+        }
+
+        public string SearchText
+        {
+            get
+            {
+                return _SearchText;
+            }
+            set
+            {
+                if (_SearchText != value)
+                {
+                    _SearchText = value;
+                    NotifyPropertyChanged(nameof(SearchText));
+                }
+            }
+        }
+
+
+        #endregion
+
+        public Ledger PLedger
+        {
+            get
+            {
+                if (_PLedger == null) _PLedger = new Ledger();
+                return _PLedger;
+            }
+            set
+            {
+                if (_PLedger != value)
+                {
+                    _PLedger = value;
+                    NotifyPropertyChanged(nameof(Ledger));
+                }
+            }
+        }
+
+
+        #region List
+        public static ObservableCollection<Ledger> LedgerList
+        {
+            get
+            {
+                return Ledger.toList;
+            }
+        }
+        #endregion
+
+        #region Master
+        public bool Save()
+        {
+            try
+            {
+                return ABClientHub.FMCGHub.Invoke<bool>("Payment_Save", this).Result;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            new Payment().toCopy<Payment>(this);
+            this.PDetail= new PaymentDetail();
+            this.PDetails = new ObservableCollection<PaymentDetail>();
+
+            PaymentDate = DateTime.Now;
+
+            NotifyAllPropertyChanged();
+        }
+
+        public bool Find()
+        {
+            try
+            {
+                Payment po = ABClientHub.FMCGHub.Invoke<Payment>("Payment_Find", SearchText).Result;
+                if (po.Id == 0) return false;
+                po.toCopy<Payment>(this);
+                this.PDetails = po.PDetails;
+                NotifyAllPropertyChanged();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete()
+        {
+            try
+            {
+                return ABClientHub.FMCGHub.Invoke<bool>("Payment_Delete", this.Id).Result;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Detail
+
+        public void SaveDetail()
+        {
+
+            PaymentDetail pod = PDetails.Where(x => x.LedgerId == PDetail.LedgerId).FirstOrDefault();
+
+            if (pod == null)
+            {
+                pod = new PaymentDetail();
+                PDetails.Add(pod);
+            }
+            else
+            {
+                PDetail.Amount = pod.Amount;
+            }
+            PDetail.toCopy<PaymentDetail>(pod);
+            ClearDetail();
+           
+
+        }
+
+        public void ClearDetail()
+        {
+            PaymentDetail pod = new PaymentDetail();
+            pod.toCopy<PaymentDetail>(PDetail);
+        }
+
+        public void DeleteDetail(string PName)
+        {
+            PaymentDetail pod = PDetails.Where(x => x.LedgerName == PName).FirstOrDefault();
+
+            if (pod != null)
+            {
+                PDetails.Remove(pod);
+                //ItemAmount = PDetails.Sum(x => x.Amount);
+            }
+        }
+        #endregion
+
+
+    
+        public bool FindRefNo()
+        {
+            var rv = false;
+            try
+            {
+                rv = ABClientHub.FMCGHub.Invoke<bool>("Find_PRef", RefNo, this).Result;
+            }
+            catch (Exception ex)
+            {
+                rv = true;
+            }
+            return rv;
+        }
+
+
+        #region Property  Changed Event
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+
+        private void NotifyAllPropertyChanged()
+        {
+            foreach (var p in this.GetType().GetProperties()) NotifyPropertyChanged(p.Name);
+        }
+
+        #endregion
+    }
+}
---
+++ AccountBuddy.SL/Hubs/Payment.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountBuddy.Common;
+
+namespace AccountBuddy.SL.Hubs
+{
+    public partial class ABServerHub
+    {
+        #region Purchase Order
+
+        #region list
+        public static List<BLL.Payment> _PPendingList;
+        public static List<BLL.Payment> PPendingList
+        {
+            get
+            {
+                if (_PPendingList == null)
+                {
+                    _PPendingList = new List<BLL.Payment>();
+                    foreach (var d1 in DB.Payments.OrderBy(x => x.RefNo).ToList())
+                    {
+                        BLL.Payment d2 = new BLL.Payment();
+                        d1.toCopy<BLL.Payment>(d2);
+                        _PPendingList.Add(d2);
+                    }
+
+                }
+                return _PPendingList;
+            }
+            set
+            {
+                _PPendingList = value;
+            }
+        }
+        #endregion
+
+
+        public bool Payment_Save(BLL.Payment PO)
+        {
+            try
+            {
+
+                DAL.Payment d = DB.Payments.Where(x => x.Id == PO.Id).FirstOrDefault();
+
+                if (d == null)
+                {
+
+                    d = new DAL.Payment();
+                    DB.Payments.Add(d);
+
+                    PO.toCopy<DAL.Payment>(d);
+
+                    foreach (var b_pod in PO.PDetails)
+                    {
+                        DAL.PaymentDetail d_pod = new DAL.PaymentDetail();
+                        b_pod.toCopy<DAL.PaymentDetail>(d_pod);
+                        d.PaymentDetails.Add(d_pod);
+                    }
+                    DB.SaveChanges();
+                    PO.Id = d.Id;
+                    LogDetailStore(PO, LogDetailType.INSERT);
+                }
+                else
+                {
+                    PO.toCopy<DAL.Payment>(d);
+                    foreach (var b_pod in PO.PDetails)
+                    {
+                        DAL.PaymentDetail d_pod = new DAL.PaymentDetail();
+                        b_pod.toCopy<DAL.PaymentDetail>(d_pod);
+                        d.PaymentDetails.Add(d_pod);
+                    }
+                    DB.SaveChanges();
+                    LogDetailStore(PO, LogDetailType.UPDATE);
+                }
+
+                BLL.Payment B_PO = PPendingList.Where(x => x.Id == PO.Id).FirstOrDefault();
+
+                if (B_PO == null)
+                {
+                    B_PO = new BLL.Payment();
+                    PPendingList.Add(B_PO);
+                }
+
+                PO.toCopy<BLL.Payment>(B_PO);
+                Clients.Clients(OtherLoginClientsOnGroup).Payment_POPendingSave(B_PO);
+
+                return true;
+            }
+            catch (Exception ex) { }
+            return false;
+        }
+
+        public BLL.Payment Payment_Find(string SearchText)
+        {
+            BLL.Payment PO = new BLL.Payment();
+            try
+            {
+
+                DAL.Payment d = DB.Payments.Where(x => x.RefNo == SearchText).FirstOrDefault();
+                DB.Entry(d).Reload();
+                if (d != null)
+                {
+
+                    d.toCopy<BLL.Payment>(PO);
+                    PO.LedgerName = (d.Ledger ?? DB.Ledgers.Find(d.LedgerId) ?? new DAL.Ledger()).LedgerName;
+                    foreach (var d_pod in d.PaymentDetails)
+                    {
+                        BLL.PaymentDetail b_pod = new BLL.PaymentDetail();
+                        d_pod.toCopy<BLL.PaymentDetail>(b_pod);
+                        PO.PDetails.Add(b_pod);
+                        b_pod.LedgerName = (d_pod.Ledger ?? DB.Ledgers.Find(d_pod.LedgerId) ?? new DAL.Ledger()).LedgerName;
+                    }
+
+                }
+            }
+            catch (Exception ex) { }
+            return PO;
+        }
+
+        public bool Payment_Delete(long pk)
+        {
+            try
+            {
+                DAL.Payment d = DB.Payments.Where(x => x.Id == pk).FirstOrDefault();
+
+                if (d != null)
+                {
+                    BLL.Payment PO = new BLL.Payment();
+                    d.toCopy<BLL.Payment>(PO);
+                    PO.LedgerName = d.Ledger.LedgerName;
+                    foreach (var d_pod in d.PaymentDetails)
+                    {
+                        BLL.PaymentDetail b_pod = new BLL.PaymentDetail();
+                        d_pod.toCopy<BLL.PaymentDetail>(b_pod);
+                        PO.PDetails.Add(b_pod);
+
+                    }
+                    DB.PaymentDetails.RemoveRange(d.PaymentDetails);
+                    DB.Payments.Remove(d);
+                    DB.SaveChanges();
+                    LogDetailStore(PO, LogDetailType.DELETE);
+
+                    BLL.Payment B_PO = PPendingList.Where(x => x.Id == PO.Id).FirstOrDefault();
+                    if (B_PO != null)
+                    {
+                        Clients.Clients(OtherLoginClientsOnGroup).Payment_POPendingDelete(B_PO.Id);
+                        PPendingList.Remove(B_PO);
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex) { }
+            return false;
+        }
+
+        public List<BLL.Payment> Payment_PPendingList()
+        {
+            return PPendingList.Where(x => x.PLedger.CompanyId== Caller.CompanyId).ToList();
+        }
+
+        public bool Find_PORef(string RefNo, BLL.Payment PO)
+
+        {
+            DAL.Payment d = DB.Payments.Where(x => x.RefNo == RefNo & x.Id != PO.Id).FirstOrDefault();
+            if (d == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        #endregion
+    }
+}
