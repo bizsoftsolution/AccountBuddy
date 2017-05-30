08421d7797c9dfcdee97f91deff214ea3a216357
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-05-30 17:20:49 +0530

    Ledger Combo Box

--- AccountBuddy.PL/frm/Transaction/frmReceipt.xaml.cs
+++ AccountBuddy.PL/frm/Transaction/frmReceipt.xaml.cs
@@ -191,6 +191,11 @@ namespace AccountBuddy.PL.frm.Transaction
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+
+            cmbDebitAC.ItemsSource = BLL.Ledger.toList.Where(x => x.AccountGroup.GroupName == "Cash-in-Hand" || x.AccountGroup.GroupName == "Bank Accounts");
+            cmbDebitAC.SelectedValuePath = "Id";
+            cmbDebitAC.DisplayMemberPath = "AccountName";
+
             btnSave.Visibility = (BLL.CompanyDetail.UserPermission.AllowInsert || BLL.CompanyDetail.UserPermission.AllowUpdate) ? Visibility.Visible : Visibility.Collapsed;
             btnDelete.Visibility = BLL.CompanyDetail.UserPermission.AllowDelete ? Visibility.Visible : Visibility.Collapsed;
 
