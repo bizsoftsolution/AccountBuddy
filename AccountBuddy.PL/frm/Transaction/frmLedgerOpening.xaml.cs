a4195339b57233e28cb605cfc4599d8cacee936e
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-05-30 19:40:21 +0530

    LOP Print

--- AccountBuddy.PL/frm/Transaction/frmLedgerOpening.xaml.cs
+++ AccountBuddy.PL/frm/Transaction/frmLedgerOpening.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Drawing.Printing;
+using System.Drawing.Imaging;
 
 namespace AccountBuddy.PL.frm.Transaction
 {
@@ -21,13 +25,22 @@ namespace AccountBuddy.PL.frm.Transaction
     /// </summary>
     public partial class frmLedgerOpening : UserControl
     {
-        List<BLL.Ledger> lstLedgerOld = new List<BLL.Ledger>();
+        List<BLL.Ledger> lstLedgerOld = new List<BLL.Ledger>(); private int m_currentPageIndex;
+        private IList<Stream> m_streams;
+
         public frmLedgerOpening()
         {
             InitializeComponent();
             RptLedger.SetDisplayMode(DisplayMode.PrintLayout);
         }
-
+        private Stream CreateStream(string name,
+    string fileNameExtension, Encoding encoding,
+    string mimeType, bool willSeek)
+        {
+            Stream stream = new MemoryStream();
+            m_streams.Add(stream);
+            return stream;
+        }
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             TabControl tc = sender as TabControl;
@@ -46,6 +59,8 @@ namespace AccountBuddy.PL.frm.Transaction
             CollectionViewSource.GetDefaultView(dgvLedger.ItemsSource).Filter = Ledger_Filter;
             CollectionViewSource.GetDefaultView(dgvLedger.ItemsSource).SortDescriptions.Add(new System.ComponentModel.SortDescription(nameof(data.AccountName), System.ComponentModel.ListSortDirection.Ascending));
             FindDiff();
+
+            LoadReport();
         }
         private bool Ledger_Filter(object obj)
         {
@@ -153,7 +168,7 @@ namespace AccountBuddy.PL.frm.Transaction
         private void rptContain_Checked(object sender, RoutedEventArgs e)
         {
             Grid_Refresh();
-        }
+        } 
 
         private void rptEndWith_Checked(object sender, RoutedEventArgs e)
         {
@@ -203,5 +218,124 @@ namespace AccountBuddy.PL.frm.Transaction
         {
             FindDiff();
         }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Warning[] warnings;
+                string[] streamids;
+                string mimeType;
+                string encoding;
+                string extension;
+
+                byte[] bytes = RptLedger.LocalReport.Render(
+                   "PDF", null, out mimeType, out encoding,
+                    out extension,
+                   out streamids, out warnings);
+
+                SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+
+                SaveFileDialog1.ShowDialog();
+                string file = string.Format(@"{0}.pdf", SaveFileDialog1.FileName);
+                FileStream fs = new FileStream(file,  
+                   FileMode.Create);
+                fs.Write(bytes, 0, bytes.Length);
+                fs.Close();
+
+                MessageBox.Show("Completed Exporting" );
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+      
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            Export(RptLedger.LocalReport);
+            Print();
+        }
+
+        private void Export(LocalReport report)
+        {
+            try
+            {
+                string deviceInfo =
+             @"<DeviceInfo>
+                <OutputFormat>EMF</OutputFormat>
+                <PageWidth>8.5in</PageWidth>
+                <PageHeight>11in</PageHeight>
+                <MarginTop>0in</MarginTop>
+                <MarginLeft>0in</MarginLeft>
+                <MarginRight>0in</MarginRight>
+                <MarginBottom>0in</MarginBottom>
+            </DeviceInfo>";
+                Warning[] warnings;
+                m_streams = new List<Stream>();
+                report.Render("Image", deviceInfo, CreateStream,
+                  out warnings);
+                foreach (Stream stream in m_streams)
+                    stream.Position = 0;
+            }
+            catch (Exception ex)
+            { }
+        }
+        private void Print()
+        {
+            try
+            {
+                if (m_streams == null || m_streams.Count == 0)
+                    throw new Exception("Error: no stream to print.");
+                PrintDocument printDoc = new PrintDocument();
+                if (!printDoc.PrinterSettings.IsValid)
+                {
+                    throw new Exception("Error: cannot find the default printer.");
+                }
+                else
+                {
+                    printDoc.PrintPage += new PrintPageEventHandler(PrintPage);
+                    m_currentPageIndex = 0;
+                    printDoc.Print();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        private void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            Metafile pageImage = new
+           Metafile(m_streams[m_currentPageIndex]);
+
+            // Adjust rectangular area with printer margins.
+            System.Drawing.Rectangle adjustedRect = new System.Drawing.Rectangle(
+            ev.PageBounds.Left - (int)ev.PageSettings.HardMarginX,
+            ev.PageBounds.Top - (int)ev.PageSettings.HardMarginY,
+            ev.PageBounds.Width,
+            ev.PageBounds.Height);
+
+            // Draw a white background for the report
+            ev.Graphics.FillRectangle(System.Drawing.Brushes.White, adjustedRect);
+
+            // Draw the report content
+            ev.Graphics.DrawImage(pageImage, adjustedRect);
+
+            // Prepare for the next page. Make sure we haven't hit the end.
+            m_currentPageIndex++;
+            ev.HasMorePages = (m_currentPageIndex < m_streams.Count);
+        }
+
+        private void btnPrintPreview_Click(object sender, RoutedEventArgs e)
+        {
+            frmLedgerOpeningPrint f = new frmLedgerOpeningPrint();
+            f.ShowDialog();
+        }
+
+        
     }
 }
