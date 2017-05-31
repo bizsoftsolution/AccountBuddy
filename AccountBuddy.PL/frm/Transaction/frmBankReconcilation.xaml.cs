f100b9f2ec38b3517a5736fbc3be1267a2f0b19e
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-05-31 12:42:34 +0530

    Bank Reconcialiation

--- AccountBuddy.PL/frm/Transaction/frmBankReconcilation.xaml.cs
+++ AccountBuddy.PL/frm/Transaction/frmBankReconcilation.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Drawing.Printing;
+using System.Drawing.Imaging;
+
 
 namespace AccountBuddy.PL.frm.Transaction
 {
@@ -20,20 +19,14 @@ namespace AccountBuddy.PL.frm.Transaction
     /// </summary>
     public partial class frmBankReconcilation : UserControl
     {
+        private int m_currentPageIndex;
+        private IList<Stream> m_streams;
+
         public frmBankReconcilation()
         {
             InitializeComponent();
-          
-        }
+            rptViewer.SetDisplayMode(DisplayMode.PrintLayout);
 
-        private void btnSearch_Click(object sender, RoutedEventArgs e)
-        {
-            if (cmbAccountName.SelectedValue != null) dgvDetails.ItemsSource = BLL.BankReconcilation.ToList((int)cmbAccountName.SelectedValue, dtpDateFrom.SelectedDate.Value, dtpDateTo.SelectedDate.Value);
-
-        }
-
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
             int yy = BLL.UserAccount.User.UserType.Company.LoginAccYear;
 
             DateTime? dtFrom = new DateTime(yy, 4, 1);
@@ -46,5 +39,238 @@ namespace AccountBuddy.PL.frm.Transaction
             cmbAccountName.DisplayMemberPath = "AccountName";
             cmbAccountName.SelectedValuePath = "Id";
         }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //LoadReport();
+        }
+
+
+        //private void LoadReport()
+        //{
+        //    try
+        //    {
+        //        List<BLL.GeneralLedger> list = BLL.GeneralLedger.ToList((int)cmbAccountName.SelectedValue, dtpDateFrom.SelectedDate.Value, dtpDateTo.SelectedDate.Value);
+        //        list = list.Select(x => new BLL.GeneralLedger()
+        //        { AccountName = x.Ledger.AccountName, Particular = x.Particular, CrAmt = x.CrAmt, DrAmt = x.DrAmt, BalAmt = x.BalAmt, EDate = x.EDate, EntryNo = x.EntryNo, EType = x.EType, Ledger = x.Ledger, RefNo = x.RefNo }).ToList();
+
+        //        try
+        //        {
+        //            rptViewer.Reset();
+        //            ReportDataSource data = new ReportDataSource("GeneralLedger", list);
+        //            ReportDataSource data1 = new ReportDataSource("CompanyDetail", BLL.CompanyDetail.toList.Where(x => x.Id == BLL.UserAccount.User.UserType.CompanyId).ToList());
+        //            rptViewer.LocalReport.DataSources.Add(data);
+        //            rptViewer.LocalReport.DataSources.Add(data1);
+        //            rptViewer.LocalReport.ReportPath = @"rpt\Report\rptGeneralLedger.rdlc";
+
+        //            ReportParameter[] par = new ReportParameter[2];
+        //            par[0] = new ReportParameter("DateFrom", dtpDateFrom.SelectedDate.Value.ToString());
+        //            par[1] = new ReportParameter("DateTo", dtpDateTo.SelectedDate.Value.ToString());
+        //            rptViewer.LocalReport.SetParameters(par);
+
+        //            rptViewer.RefreshReport();
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+
+        //}
+
+        //private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    TabControl tc = sender as TabControl;
+
+        //    if (tc.SelectedIndex == 1)
+        //    {
+        //        LoadReport();
+        //    }
+
+        //}
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbAccountName.SelectedValue == null)
+            {
+                MessageBox.Show("Enter AccountName..");
+                cmbAccountName.Focus();
+            }
+            else
+            {
+                if (cmbAccountName.SelectedValue != null) dgvBankReconciliation.ItemsSource = BLL.BankReconcilation.ToList((int)cmbAccountName.SelectedValue, dtpDateFrom.SelectedDate.Value, dtpDateTo.SelectedDate.Value);
+                //LoadReport();
+            }
+
+        }
+
+        private void dgvBankReconciliation_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var gl = dgvBankReconciliation.SelectedItem as BLL.BankReconcilation;
+            if (gl != null)
+            {
+                if (gl.EType == 'P')
+                {
+                    Transaction.frmPayment f = new Transaction.frmPayment();
+                    App.frmHome.ShowForm(f);
+                    System.Windows.Forms.Application.DoEvents();
+                    f.data.SearchText = gl.EntryNo;
+                    System.Windows.Forms.Application.DoEvents();
+                    f.data.Find();
+                }
+                else if (gl.EType == 'R')
+                {
+                    Transaction.frmReceipt f = new Transaction.frmReceipt();
+                    App.frmHome.ShowForm(f);
+                    System.Windows.Forms.Application.DoEvents();
+                    f.data.SearchText = gl.EntryNo;
+                    System.Windows.Forms.Application.DoEvents();
+                    f.data.Find();
+                }
+                else if (gl.EType == 'J')
+                {
+                    Transaction.frmJournal f = new Transaction.frmJournal();
+                    App.frmHome.ShowForm(f);
+                    System.Windows.Forms.Application.DoEvents();
+                    f.data.SearchText = gl.EntryNo;
+                    System.Windows.Forms.Application.DoEvents();
+                    f.data.Find();
+                }
+            }
+        }
+
+        #region Button Events
+        private Stream CreateStream(string name,
+  string fileNameExtension, Encoding encoding,
+  string mimeType, bool willSeek)
+        {
+            Stream stream = new MemoryStream();
+            m_streams.Add(stream);
+            return stream;
+        }
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Warning[] warnings;
+                string[] streamids;
+                string mimeType;
+                string encoding;
+                string extension;
+
+                byte[] bytes = rptViewer.LocalReport.Render(
+                   "PDF", null, out mimeType, out encoding,
+                    out extension,
+                   out streamids, out warnings);
+
+                SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+
+                SaveFileDialog1.ShowDialog();
+                string file = string.Format(@"{0}.pdf", SaveFileDialog1.FileName);
+                FileStream fs = new FileStream(file,
+                   FileMode.Create);
+                fs.Write(bytes, 0, bytes.Length);
+                fs.Close();
+
+                //MessageBox.Show("Completed Exporting");
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            Export(rptViewer.LocalReport);
+            Print();
+        }
+
+        private void Export(LocalReport report)
+        {
+            try
+            {
+                string deviceInfo =
+             @"<DeviceInfo>
+                <OutputFormat>EMF</OutputFormat>
+                <PageWidth>8.5in</PageWidth>
+                <PageHeight>11in</PageHeight>
+                <MarginTop>0in</MarginTop>
+                <MarginLeft>0in</MarginLeft>
+                <MarginRight>0in</MarginRight>
+                <MarginBottom>0in</MarginBottom>
+            </DeviceInfo>";
+                Warning[] warnings;
+                m_streams = new List<Stream>();
+                report.Render("Image", deviceInfo, CreateStream,
+                  out warnings);
+                foreach (Stream stream in m_streams)
+                    stream.Position = 0;
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        private void Print()
+        {
+            try
+            {
+                if (m_streams == null || m_streams.Count == 0)
+                    throw new Exception("Error: no stream to print.");
+                PrintDocument printDoc = new PrintDocument();
+                if (!printDoc.PrinterSettings.IsValid)
+                {
+                    throw new Exception("Error: cannot find the default printer.");
+                }
+                else
+                {
+                    printDoc.PrintPage += new PrintPageEventHandler(PrintPage);
+                    m_currentPageIndex = 0;
+                    printDoc.Print();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            Metafile pageImage = new
+           Metafile(m_streams[m_currentPageIndex]);
+
+            // Adjust rectangular area with printer margins.
+            System.Drawing.Rectangle adjustedRect = new System.Drawing.Rectangle(
+            ev.PageBounds.Left - (int)ev.PageSettings.HardMarginX,
+            ev.PageBounds.Top - (int)ev.PageSettings.HardMarginY,
+            ev.PageBounds.Width,
+            ev.PageBounds.Height);
+
+            // Draw a white background for the report
+            ev.Graphics.FillRectangle(System.Drawing.Brushes.White, adjustedRect);
+
+            // Draw the report content
+            ev.Graphics.DrawImage(pageImage, adjustedRect);
+
+            // Prepare for the next page. Make sure we haven't hit the end.
+            m_currentPageIndex++;
+            ev.HasMorePages = (m_currentPageIndex < m_streams.Count);
+        }
+
+        private void btnPrintPreview_Click(object sender, RoutedEventArgs e)
+        {
+            //frmGeneralLedgerPrint f = new frmGeneralLedgerPrint();
+            //f.LoadReport((int)cmbAccountName.SelectedValue, dtpDateFrom.SelectedDate.Value, dtpDateTo.SelectedDate.Value);
+            //f.ShowDialog();
+        }
+
+        #endregion
     }
 }
--- AccountBuddy.SL/Hubs/BankReconcilation.cs
+++ AccountBuddy.SL/Hubs/BankReconcilation.cs
@@ -11,12 +11,12 @@ namespace AccountBuddy.SL.Hubs
     {
         public List<BLL.BankReconcilation> BankReconcilation_List(int LedgerId, DateTime dtFrom, DateTime dtTo)
         {
-            List<BLL.BankReconcilation> lstGeneralLedger = new List<BLL.BankReconcilation>();
+            List<BLL.BankReconcilation> lstBankReconcilation = new List<BLL.BankReconcilation>();
 
 
             BLL.BankReconcilation gl = new BLL.BankReconcilation();
 
-            var lstLedger = DB.Ledgers.Where(x => x.AccountGroup.CompanyDetail.Id == Caller.CompanyId && x.Id == LedgerId).ToList();
+            var lstLedger = DB.Ledgers.Where(x => x.AccountGroup.CompanyDetail.Id == Caller.CompanyId && x.Id == LedgerId ).ToList();
             decimal TotDr = 0, TotCr = 0;
 
             #region Ledger
@@ -26,9 +26,8 @@ namespace AccountBuddy.SL.Hubs
             foreach (var l in lstLedger)
             {
                 gl = new BLL.BankReconcilation();
-                gl.Ledger = new BLL.Ledger();
 
-                l.toCopy<BLL.Ledger>(gl.Ledger);
+                gl.Ledger = LedgerDAL_BLL(l);
 
                 OPDr = l.OPDr ?? 0;
                 OPCr = l.OPCr ?? 0;
@@ -59,14 +58,15 @@ namespace AccountBuddy.SL.Hubs
                 gl.BalAmt = BalAmt;
 
                 gl.Ledger = new BLL.Ledger();
-                gl.Ledger.LedgerName = string.Format("Balance {0}", l.LedgerName);
-                lstGeneralLedger.Add(gl);
+                gl.Particular = string.Format("Balance {0}", l.LedgerName);
+                lstBankReconcilation.Add(gl);
 
-                foreach (var pd in l.PaymentDetails.Where(x => x.Payment.PaymentDate >= dtFrom && x.Payment.PaymentDate <= dtTo && x.Payment.Status == "Proccess").ToList())
+                foreach (var pd in l.PaymentDetails.Where(x => x.Payment.PaymentDate >= dtFrom && x.Payment.PaymentDate <= dtTo).ToList())
                 {
                     gl = new BLL.BankReconcilation();
                     gl.Ledger = new BLL.Ledger();
-                    pd.Ledger.toCopy<BLL.Ledger>(gl.Ledger);
+                    gl.Ledger = LedgerDAL_BLL(pd.Payment.Ledger);
+                    gl.Particular = pd.Particular;
                     gl.EId = pd.Payment.Id;
                     gl.EType = 'P';
                     gl.EDate = pd.Payment.PaymentDate;
@@ -76,48 +76,58 @@ namespace AccountBuddy.SL.Hubs
                     gl.CrAmt = 0;
                     BalAmt += (gl.DrAmt - gl.CrAmt);
                     gl.BalAmt = BalAmt;
-                    lstGeneralLedger.Add(gl);
+                    lstBankReconcilation.Add(gl);
                 }
 
 
-                foreach (var p in l.Payments.Where(x => x.PaymentDate >= dtFrom && x.PaymentDate <= dtTo && x.Status == "Proccess").ToList())
+                foreach (var p in l.Payments.Where(x => x.PaymentDate >= dtFrom && x.PaymentDate <= dtTo).ToList())
                 {
-                    gl = new BLL.BankReconcilation();
-                    gl.Ledger = new BLL.Ledger();
-                    p.Ledger.toCopy<BLL.Ledger>(gl.Ledger);
-                    gl.EId = p.Id;
-                    gl.EType = 'P';
-                    gl.EDate = p.PaymentDate;
-                    gl.RefNo = p.PaymentMode == "Cheque" ? p.ChequeNo : p.RefNo;
-                    gl.EntryNo = p.EntryNo;
-                    gl.DrAmt = 0;
-                    gl.CrAmt = p.Amount;
-                    BalAmt += (gl.DrAmt - gl.CrAmt);
-                    gl.BalAmt = BalAmt;
-                    lstGeneralLedger.Add(gl);
+                    foreach (var pd in p.PaymentDetails)
+                    {
+                        gl = new BLL.BankReconcilation();
+                        gl.Ledger = new BLL.Ledger();
+                        gl.Ledger = LedgerDAL_BLL(pd.Ledger);
+                        gl.Particular = pd.Particular;
+                        gl.EId = p.Id;
+                        gl.EType = 'P';
+                        gl.EDate = p.PaymentDate;
+                        gl.RefNo = p.PaymentMode == "Cheque" ? p.ChequeNo : p.RefNo;
+                        gl.EntryNo = p.EntryNo;
+                        gl.DrAmt = 0;
+                        gl.CrAmt = pd.Amount;
+                        BalAmt += (gl.DrAmt - gl.CrAmt);
+                        gl.BalAmt = BalAmt;
+                        lstBankReconcilation.Add(gl);
+                    }
                 }
 
-                foreach (var r in l.Receipts.Where(x => x.ReceiptDate >= dtFrom && x.ReceiptDate <= dtTo && x.Status == "Process").ToList())
+                foreach (var r in l.Receipts.Where(x => x.ReceiptDate >= dtFrom && x.ReceiptDate <= dtTo).ToList())
                 {
-                    gl = new BLL.BankReconcilation();
-                    gl.Ledger = new BLL.Ledger();
-                    r.Ledger.toCopy<BLL.Ledger>(gl.Ledger);
-                    gl.EId = r.Id;
-                    gl.EType = 'R';
-                    gl.EDate = r.ReceiptDate;
-                    gl.RefNo = r.ReceiptMode == "Cheque" ? r.ChequeNo : r.RefNo;
-                    gl.EntryNo = r.EntryNo;
-                    gl.DrAmt = r.Amount;
-                    gl.CrAmt = 0;
-                    BalAmt += (gl.DrAmt - gl.CrAmt);
-                    gl.BalAmt = BalAmt;
-                    lstGeneralLedger.Add(gl);
+                    foreach (var rd in r.ReceiptDetails)
+                    {
+                        gl = new BLL.BankReconcilation();
+                        gl.Ledger = new BLL.Ledger();
+                        gl.Ledger = LedgerDAL_BLL(rd.Ledger);
+                        gl.Particular = rd.Particulars;
+                        gl.EId = r.Id;
+                        gl.EType = 'R';
+                        gl.EDate = r.ReceiptDate;
+                        gl.RefNo = r.ReceiptMode == "Cheque" ? r.ChequeNo : r.RefNo;
+                        gl.EntryNo = r.EntryNo;
+                        gl.DrAmt = rd.Amount;
+                        gl.CrAmt = 0;
+                        BalAmt += (gl.DrAmt - gl.CrAmt);
+                        gl.BalAmt = BalAmt;
+                        lstBankReconcilation.Add(gl);
+                    }
+
                 }
-                foreach (var rd in l.ReceiptDetails.Where(x => x.Receipt.ReceiptDate >= dtFrom && x.Receipt.ReceiptDate <= dtTo && x.Receipt.Status=="Process").ToList())
+                foreach (var rd in l.ReceiptDetails.Where(x => x.Receipt.ReceiptDate >= dtFrom && x.Receipt.ReceiptDate <= dtTo).ToList())
                 {
                     gl = new BLL.BankReconcilation();
                     gl.Ledger = new BLL.Ledger();
-                    rd.Ledger.toCopy<BLL.Ledger>(gl.Ledger);
+                    gl.Ledger = LedgerDAL_BLL(rd.Receipt.Ledger);
+                    gl.Particular = rd.Particulars;
                     gl.EId = rd.Receipt.Id;
                     gl.EType = 'R';
                     gl.EDate = rd.Receipt.ReceiptDate;
@@ -127,38 +137,40 @@ namespace AccountBuddy.SL.Hubs
                     gl.CrAmt = rd.Amount;
                     BalAmt += (gl.DrAmt - gl.CrAmt);
                     gl.BalAmt = BalAmt;
-                    lstGeneralLedger.Add(gl);
+                    lstBankReconcilation.Add(gl);
                 }
 
-                //foreach (var jd in l.JournalDetails.Where(x => x.Journal.JournalDate >= dtFrom && x.Journal.JournalDate <= dtTo).ToList())
-                //{
-                //    gl = new BLL.BankReconcilation();
-                //    gl.Ledger = new BLL.Ledger();
-                //    jd.Ledger.toCopy<BLL.Ledger>(gl.Ledger);
-                //    gl.EId = jd.Journal.Id;
-                //    gl.EType = 'J';
-                //    gl.EDate = jd.Journal.JournalDate;
-                //    gl.RefNo = "";
-                //    gl.EntryNo = jd.Journal.EntryNo;
-                //    gl.DrAmt = jd.DrAmt;
-                //    gl.CrAmt = jd.CrAmt;
-                //    BalAmt += (gl.DrAmt - gl.CrAmt);
-                //    gl.BalAmt = BalAmt;
-                //    lstGeneralLedger.Add(gl);
-                //}
-                //gl = new BLL.BankReconcilation();
-                //gl.Ledger = new BLL.Ledger();
-                //gl.Ledger.LedgerName = "Total";
-                //gl.DrAmt = lstGeneralLedger.Sum(x => x.DrAmt);
-                //gl.CrAmt = lstGeneralLedger.Sum(x => x.CrAmt);
-                //gl.BalAmt = BalAmt;
-                //lstGeneralLedger.Add(gl);
+                foreach (var jd in l.JournalDetails.Where(x => x.Journal.JournalDate >= dtFrom && x.Journal.JournalDate <= dtTo).ToList())
+                {
+                    gl = new BLL.BankReconcilation();
+                    gl.Ledger = new BLL.Ledger();
+
+                    gl.Ledger = LedgerDAL_BLL(jd.Journal.JournalDetails.Where(x => (jd.DrAmt != 0 && x.CrAmt != 0) || (jd.CrAmt != 0 && x.DrAmt != 0)).FirstOrDefault().Ledger);
+                    gl.Particular = jd.Particulars;
+                    gl.EId = jd.Journal.Id;
+                    gl.EType = 'J';
+                    gl.EDate = jd.Journal.JournalDate;
+                    gl.RefNo = "";
+                    gl.EntryNo = jd.Journal.EntryNo;
+                    gl.DrAmt = jd.DrAmt;
+                    gl.CrAmt = jd.CrAmt;
+                    BalAmt += (gl.DrAmt - gl.CrAmt);
+                    gl.BalAmt = BalAmt;
+                    lstBankReconcilation.Add(gl);
+                }
+                gl = new BLL.BankReconcilation();
+                gl.Ledger = new BLL.Ledger();
+                gl.Particular = "Total";
+                gl.DrAmt = lstBankReconcilation.Sum(x => x.DrAmt);
+                gl.CrAmt = lstBankReconcilation.Sum(x => x.CrAmt);
+                gl.BalAmt = BalAmt;
+                lstBankReconcilation.Add(gl);
 
             }
             #endregion
 
 
-            return lstGeneralLedger;
+            return lstBankReconcilation;
         }
 
     }
