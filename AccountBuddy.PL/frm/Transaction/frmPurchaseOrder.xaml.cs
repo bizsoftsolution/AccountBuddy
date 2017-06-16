b54a7c1a28e52124110a7e4e42ef0d75086e7419
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-06-16 11:12:29 +0530

    PO SOStatus

--- AccountBuddy.PL/frm/Transaction/frmPurchaseOrder.xaml.cs
+++ AccountBuddy.PL/frm/Transaction/frmPurchaseOrder.xaml.cs
@@ -219,5 +219,10 @@ namespace AccountBuddy.PL.frm.Transaction
             textBox.SelectionStart = selectionStart <= textBox.Text.Length ? selectionStart : textBox.Text.Length;
 
         }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
