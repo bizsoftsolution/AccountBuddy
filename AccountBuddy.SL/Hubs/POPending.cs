108e59d6b92dfabec9371b7e0c93b8312e629b69
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-06-16 12:36:14 +0530

    Sales Order And Purchase Order Pending

---
+++ AccountBuddy.SL/Hubs/POPending.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountBuddy.SL.Hubs
+{
+    public partial class ABServerHub
+    {
+        public List<BLL.POPending> POPending_List(DateTime dtFrom, DateTime dtTo)
+        {
+            List<BLL.POPending> lstPOPending = new List<BLL.POPending>();
+            BLL.POPending tb = new BLL.POPending();
+
+            var lstLedger = DB.Ledgers.Where(x => x.AccountGroup.CompanyId == Caller.CompanyId).ToList();
+           
+            foreach (var l in lstLedger)
+            {
+                if(l.PurchaseOrders.Where(x => x.PODate >= dtFrom && x.PODate <= dtTo).Count()!=0)
+                {
+                    var po = l.PurchaseOrders.FirstOrDefault();
+                    tb = new BLL.POPending();
+                    tb.Ledger = LedgerDAL_BLL(l);
+
+                    tb.EntryNo = po.RefNo;
+                    tb.Amount = po.TotalAmount;
+                    tb.PODate = po.PODate;
+                    tb.Status = "Pending";
+                    lstPOPending.Add(tb);
+                }
+               
+
+               
+               
+            }
+
+
+
+            return lstPOPending ;
+        }
+
+    }
+}
---
+++ AccountBuddy.SL/Hubs/SOPending.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountBuddy.SL.Hubs
+{
+    public partial class ABServerHub
+    {
+        public List<BLL.SOPending> SOPending_List(DateTime dtFrom, DateTime dtTo)
+        {
+            List<BLL.SOPending> lstSOPending = new List<BLL.SOPending>();
+            BLL.SOPending tb = new BLL.SOPending();
+
+            var lstLedger = DB.Ledgers.Where(x => x.AccountGroup.CompanyId == Caller.CompanyId).ToList();
+
+            foreach (var l in lstLedger)
+            {
+                if (l.SalesOrders.Where(x => x.SODate >= dtFrom && x.SODate <= dtTo).Count() != 0)
+                {
+                    var po = l.PurchaseOrders.FirstOrDefault();
+                    tb = new BLL.SOPending();
+                    tb.Ledger = LedgerDAL_BLL(l);
+
+                    tb.EntryNo = po.RefNo;
+                    tb.Amount = po.TotalAmount;
+                    tb.SODate = po.PODate;
+                    tb.Status = "Pending";
+                    lstSOPending.Add(tb);
+                }
+
+
+
+
+            }
+
+
+
+            return lstSOPending;
+        }
+
+    }
+}
