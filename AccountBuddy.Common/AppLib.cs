e4fcde1a9b2ed8597e98a6ca7e4b5cf58ac6308a
Bala <bala.bizsoft@gmail.com>
2017-08-14 16:29:43 +0530

    fixed menu search in PL

--- AccountBuddy.Common/AppLib.cs
+++ AccountBuddy.Common/AppLib.cs
@@ -17,6 +17,7 @@ namespace AccountBuddy.Common
         public static decimal GSTPer = (decimal)0.06;
 
         public static string AppName = "";
+        public static string FundName = "";
       
 
         public static string conString = @"Data source=.\sqlexpress;initial catalog=DBAccountBuddy;integrated security=True";
