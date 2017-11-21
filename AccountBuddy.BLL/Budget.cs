cdf32fbfea99cb4acac5480a5089728223845833
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-11-21 16:10:20 +0530

    request report

---
+++ AccountBuddy.BLL/Budget.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBuddy.BLL
+{
+    public class Budget : INotifyPropertyChanged
+    {
+
+        #region Fields
+
+
+        private string _Department;
+        private string _Budgets;
+        private string _Approved;
+        private decimal _Balance;
+        private string _Request;
+        private string _Remaining;
+
+
+        private static UserTypeDetail _UserPermission;
+        #endregion
+
+        #region Property
+
+
+        public string Department
+        {
+            get
+            {
+                return _Department;
+            }
+            set
+            {
+                if (_Department != value)
+                {
+                    _Department = value;
+                    NotifyPropertyChanged(nameof(Department));
+
+                }
+            }
+        }
+
+        public string Budgets
+        {
+            get
+            {
+                return _Budgets;
+            }
+            set
+            {
+                if (_Budgets != value)
+                {
+                    _Budgets = value;
+                    NotifyPropertyChanged(nameof(Budgets));
+
+                }
+            }
+        }
+
+        public string Approved
+        {
+            get
+            {
+                return _Approved;
+            }
+            set
+            {
+                if (_Approved != value)
+                {
+                    _Approved = value;
+                    NotifyPropertyChanged(nameof(Approved));
+
+                }
+            }
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return _Balance;
+            }
+            set
+            {
+                if (_Balance != value)
+                {
+                    _Balance = value;
+                    NotifyPropertyChanged(nameof(Balance));
+
+                }
+            }
+        }
+
+
+        public string Request
+        {
+            get
+            {
+                return _Request;
+            }
+            set
+            {
+                if (_Request != value)
+                {
+                    _Request = value;
+                    NotifyPropertyChanged(nameof(Request));
+
+                }
+            }
+        }
+
+        public string Remaining
+        {
+            get
+            {
+                return _Remaining;
+            }
+            set
+            {
+                if (_Remaining != value)
+                {
+                    _Remaining = value;
+                    NotifyPropertyChanged(nameof(Remaining));
+
+                }
+            }
+        }
+
+
+        #endregion
+
+        #region Property  Changed Event
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private void NotifyAllPropertyChanged()
+        {
+            foreach (var p in this.GetType().GetProperties()) NotifyPropertyChanged(p.Name);
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ AccountBuddy.BLL/RequestReport.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBuddy.BLL
+{
+  public  class RequestReport : INotifyPropertyChanged
+    {
+
+        #region Fields
+      
+        private DateTime? _RequestAt;
+        private string _RequestBy;
+        private string _RequestTo;
+        private decimal _Amount;
+        private string _Particulars;
+        private string _Status;
+    
+
+        private static UserTypeDetail _UserPermission;
+        #endregion
+
+        #region Property
+
+      
+        public DateTime? RequestAt
+        {
+            get
+            {
+                return _RequestAt;
+            }
+            set
+            {
+                if (_RequestAt != value)
+                {
+                    _RequestAt = value;
+                    NotifyPropertyChanged(nameof(RequestAt));
+
+                }
+            }
+        }
+        public string RequestBy
+        {
+            get
+            {
+                return _RequestBy;
+            }
+            set
+            {
+                if (_RequestBy != value)
+                {
+                    _RequestBy = value;
+                    NotifyPropertyChanged(nameof(RequestBy));
+
+                }
+            }
+        }
+        public string RequestTo
+        {
+            get
+            {
+                return _RequestTo;
+            }
+            set
+            {
+                if (_RequestTo != value)
+                {
+                    _RequestTo = value;
+                    NotifyPropertyChanged(nameof(RequestTo));
+
+                }
+            }
+        }
+        public string Particulars
+        {
+            get
+            {
+                return _Particulars;
+            }
+            set
+            {
+                if (_Particulars != value)
+                {
+                    _Particulars = value;
+                    NotifyPropertyChanged(nameof(Particulars));
+
+                }
+            }
+        }
+
+        public decimal Amount
+        {
+            get
+            {
+                return _Amount;
+            }
+            set
+            {
+                if (_Amount != value)
+                {
+                    _Amount = value;
+                    NotifyPropertyChanged(nameof(Amount));
+
+                }
+            }
+        }
+
+        public string Department
+        {
+            get
+            {
+                return Particulars;
+            }
+            set
+            {
+                if (Particulars != value)
+                {
+                    Particulars = value;
+                    NotifyPropertyChanged(nameof(Department));
+
+                }
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    NotifyPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+  
+
+        #endregion
+
+        #region Property  Changed Event
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private void NotifyAllPropertyChanged()
+        {
+            foreach (var p in this.GetType().GetProperties()) NotifyPropertyChanged(p.Name);
+        }
+
+        #endregion
+
+      
+
+    }
+}
+
