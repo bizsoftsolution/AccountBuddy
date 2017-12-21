7b9afbab1446d2b818c8dd4d8e0254026408928d
Youvaniga Palanisamy <youvaniga.bizsoft@gmail.com>
2017-12-21 12:11:17 +0530

    Purchase order

--- AccountBuddy.SL/Hubs/SalesOrder.cs
+++ AccountBuddy.SL/Hubs/SalesOrder.cs
@@ -250,7 +250,7 @@ namespace AccountBuddy.SL.Hubs
                 string RefCode = string.Format("{0}{1}", BLL.FormPrefix.PurchaseOrder, P.Id);
                 if (P.Ledger == null)
                 {
-                    P.Ledger = DB.Ledgers.Where(x => x.Id == P.LedgerId && x.AccountGroup.CompanyId == P.PurchaseOrderDetails.Select(y => y.Product.StockGroup.CompanyId).FirstOrDefault()).FirstOrDefault();
+                    P.Ledger = DB.Ledgers.Where(x => x.Id == P.LedgerId).FirstOrDefault();
                 }
                 DAL.SalesOrder s = DB.SalesOrders.Where(x => x.RefCode == RefCode).FirstOrDefault();
                 if (P.Ledger.LedgerName.StartsWith("CM-") || P.Ledger.LedgerName.StartsWith("WH-") || P.Ledger.LedgerName.StartsWith("DL-"))
